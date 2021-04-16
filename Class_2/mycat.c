a0158be82acd73b698017d0650d9b3c5a0d98178
wangrong <sszwangrong@126.com>
2021-04-16 22:29:33 +0800

    fix some bugs

--- Class_2/mycat.c
+++ Class_2/mycat.c
@@ -10,23 +10,37 @@ void panic(char* message) {
 	exit(EXIT_FAILURE);
 }
 
-int fd;
 
-
-
-int main(int argc,char* argv[]) {
+void catfile(char *src) {
+	int fd;
+	
 	char str[255];
 	memset(str,'\0',255);
-	fd = open(argv[1],O_RDONLY);
+	
+	fd = open(src, O_RDONLY);
 	if(fd < 0 ) panic("open");
 	int num;
 	num = read(fd,str,sizeof(str));
-	while(num>0) {
-		printf("%s",str);
-		memset(str,'\0',255);
-		num = read(fd,str,sizeof(str));
+	while(num > 0) {
+		printf("%s", str);
+		memset(str, '\0', 255);
+		num = read(fd, str, sizeof(str));
 	}
 	num = close(fd);
+	
 	if(num < 0) panic("close");
+	return ;
+}
+
+int main(int argc,char* argv[]) {
+	if(argc < 2) {
+		printf("参数不够!!\n");
+		return 0;
+	}
+	int i;
+	for(i = 1; i < argc; i++)  {
+		catfile(argv[i]);
+	}
+	
 	return 0;
 }
--- Class_2/mycp.c
+++ Class_2/mycp.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <utime.h>
 
 void panic(char* message) {
 	perror(message);
@@ -15,9 +15,16 @@ int fdSrc;
 int fdRes;
 
 int main(int agrc,char* argv[]) {
-	if(agrc < 3) panic("argv is not full");
-	
+	if(agrc != 3) panic("argv error\n");	
 	char str[255];
+
+	//首先判断两个文件是否重名
+	if(0 == strcmp(argv[1],argv[2])) {
+		printf("文件已存在！！！\n");
+		return 1;
+	}
+
+
 	memset(str,'\0',255);
 	fdSrc = open(argv[1],O_RDONLY);
 	if(fdSrc < 0) panic("open Src");
@@ -29,12 +36,32 @@ int main(int agrc,char* argv[]) {
 	int num;
 	num = read(fdSrc,str,sizeof(str));
 	while(num > 0) {
-		write(fdRes,str,sizeof(str));
+		write(fdRes,str,strlen(str));
 		memset(str,'\0',255);
 		num = read(fdSrc,str,sizeof(str));
 	}
+
 	close(fdSrc);
 	close(fdRes);
 
-	return 0;
+	//获取源文件属性
+	struct stat src_stat;
+    	stat(argv[1], &src_stat);
+ 
+       //修改目标文件时间
+       struct utimbuf timbuf;
+       timbuf.actime = src_stat.st_atime;	//访问时间
+       timbuf.modtime = src_stat.st_mtime;      //修改时间
+       utime(argv[2], &timbuf);
+ 
+       //修改权限
+       chmod(argv[2], src_stat.st_mode);
+ 
+       // 修改所有者
+       int ret = chown(argv[2], src_stat.st_uid, src_stat.st_gid);
+       if(ret < 0)
+       {
+	       perror("chown");
+       }
+       return 0;
 }
