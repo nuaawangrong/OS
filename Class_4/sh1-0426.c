3b3351b01b78eca63c7a0c58c4bc4166bff700ec
wangrong <sszwangrong@126.com>
2021-04-26 09:23:44 -0400

    根据冉聪提供的建议,修复了几个bug. 1.cd ~: 	由getpwuid函数获取home路径,替换掉~符号. 2.新增replaceHomePath函数用于替换~符号. 3.cd -: 	使用全局变量prePath存储前一次的工作路径,getcwd函数获取当前工作路径.

--- Class_4/sh1-0426.c
+++ Class_4/sh1-0426.c
@@ -5,12 +5,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <pwd.h>
 
-//使用全局变量保存上一次的工作路径
-char prePath[255] = "\0";
-
-//分解command命令的各个参数
 int commandSplit(char **argv,char *command)
 {
 
@@ -44,23 +39,6 @@ int commandSplit(char **argv,char *command)
 	return cnt;
 }
 
-//将~符号替换为home路径
-char *replaceHomePath(char *srcPath, char *homePath)
-{
-	char *res = NULL;
-	res = (char*)malloc(strlen(srcPath)+strlen(homePath)+1);
-	memset(res,0,sizeof(res));
-
-	strcat(res,homePath);
-	char *p = srcPath;
-
-	strcat(res,p+1);
-
-	free(srcPath);
-
-	return res;
-}
-
 void mysys(char *command)
 {
 	//实现该函数，该函数执行一条命令，并等待该命令执行结束
@@ -69,42 +47,10 @@ void mysys(char *command)
 
 	cnt = commandSplit(argv,command);
 	
-	//对~进行处理,将其替换为home路径
-	if(argv[1] != NULL && argv[1][0] == '~')
-	{
-		struct passwd *user;
-		user= getpwuid(getuid());
-		argv[1] = replaceHomePath(argv[1],user->pw_dir);
-	}
-
-	
 	//cd命令直接在父进程中执行，调用chdir函数
 	if(!strcmp(argv[0],"cd"))
 	{
-		int ret = -1;
-
-		//使用一个临时变量存储prePath
-		char *tempPath = (char*)malloc(strlen(prePath)+1);
-		memset(tempPath,0,sizeof(tempPath));
-		strcat(tempPath,prePath);
-
-		//保存当前的工作路径
-		memset(prePath,0,sizeof(prePath));
-		getcwd(prePath,sizeof(prePath));
-
-
-		if(!strcmp(argv[1],"-"))
-		{	
-			//处理cd -.
-			ret = chdir(tempPath);
-		}
-		else
-		{
-			ret = chdir(argv[1]);
-		}
-
-		free(tempPath);
-
+		int ret = chdir(argv[1]);
 		if(-1 == ret)
 		{
 			perror("cd");
@@ -140,26 +86,22 @@ void mysys(char *command)
 }
 
 int main()
+
 {
-	
 	char order[255];
 
 	while(1)
 	{
-
 		printf("> ");
 		memset(order,0,255);
 		fgets(order,255,stdin);
 
-		//回车单独处理
-		if(!strcmp(order,"\n")) continue;
-		
 		//去掉换行符
 		order[strlen(order)-1] = 0;
 	
 		//退出命令单独处理	
 		if(!strcmp(order,"exit")) break;
-
+		
 		mysys(order);
 	}
 	
