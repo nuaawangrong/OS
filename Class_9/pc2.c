2d8a7ce38c9ad28fda8e33ba98400831989ea32b
wangrong <sszwangrong@126.com>
2021-06-17 14:23:41 +0800

    Finish lab9 fully!.Fixed some bugs of lab9-pc1.c.

--- Class_9/pc2.c
+++ Class_9/pc2.c
@@ -58,13 +58,44 @@ void put_item2(int item2)
 	buffer2[in2] = item2;
 	in2 = (in2 + 1)%CAPACITY;
 }
-pthread_mutex_t mutex1;
-pthread_cond_t wait_empty_buffer1;
-pthread_cond_t wait_full_buffer1;
 
-pthread_mutex_t mutex2;
-pthread_cond_t wait_empty_buffer2;
-pthread_cond_t wait_full_buffer2;
+typedef struct {
+    int value;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+} sema_t;
+
+void sema_init(sema_t *sema, int value)
+{
+    sema->value = value;
+    pthread_mutex_init(&sema->mutex, NULL);
+    pthread_cond_init(&sema->cond, NULL);
+}
+
+void sema_wait(sema_t *sema)
+{
+    pthread_mutex_lock(&sema->mutex);
+    while (sema->value <= 0)
+        pthread_cond_wait(&sema->cond, &sema->mutex);
+    sema->value--;
+    pthread_mutex_unlock(&sema->mutex);
+}
+
+void sema_signal(sema_t *sema)
+{
+    pthread_mutex_lock(&sema->mutex);
+    ++sema->value;
+    pthread_cond_signal(&sema->cond);
+    pthread_mutex_unlock(&sema->mutex);
+}
+
+sema_t mutex_sema1;
+sema_t empty_buffer1_sema;
+sema_t full_buffer1_sema;
+
+sema_t mutex_sema2;
+sema_t empty_buffer2_sema;
+sema_t full_buffer2_sema;
 
 #define ITEM_COUNT (CAPACITY*2)
 
@@ -73,17 +104,18 @@ void *consume(void *arg)
 {
 	int i;
 	int item;
+	
 	for(i=0;i<ITEM_COUNT;i++)
 	{
-		pthread_mutex_lock(&mutex2);
-		while (buffer2_is_empty())
-			pthread_cond_wait(&wait_full_buffer2, &mutex2);
+		sema_wait(&full_buffer2_sema);
+		sema_wait(&mutex_sema2);
+		
 
 		item = get_item2();
 		printf("        consume item: %c\n",item);
 
-		pthread_cond_signal(&wait_empty_buffer2);
-		pthread_mutex_unlock(&mutex2);
+		sema_signal(&mutex_sema2);
+		sema_signal(&empty_buffer2_sema);
 	}
 	return NULL;
 }
@@ -96,29 +128,27 @@ void *calculate(void *arg)
 	for(i=0;i<ITEM_COUNT;i++)
 	{
 		//从buffer1中读入字符
-		pthread_mutex_lock(&mutex1);
-		while (buffer1_is_empty())
-			pthread_cond_wait(&wait_full_buffer1, &mutex1);
+		sema_wait(&full_buffer1_sema);
+		sema_wait(&mutex_sema1);
 
 		item = get_item1();
 		//printf("    calc get item1: %c\n",item);
 
-		pthread_cond_signal(&wait_empty_buffer1);
-		pthread_mutex_unlock(&mutex1);
+		sema_signal(&mutex_sema1);
+		sema_signal(&empty_buffer1_sema);
 		
 		//将小写字符转换为大写字符
 		item -= 32;
 
 		//从buffer2中写入字符
-		pthread_mutex_lock(&mutex2);
-		while (buffer2_is_full())
-			pthread_cond_wait(&wait_empty_buffer2, &mutex2);
+		sema_wait(&empty_buffer2_sema);
+		sema_wait(&mutex_sema2);
 
 		put_item2(item);
 		//printf("    calc put item2: %c\n",item);
 
-		pthread_cond_signal(&wait_full_buffer2);
-		pthread_mutex_unlock(&mutex2);
+		sema_signal(&mutex_sema2);
+		sema_signal(&full_buffer2_sema);
 
 	}
 	return NULL;
@@ -132,16 +162,17 @@ void *produce(void *arg)
 
 	for(i=0;i<ITEM_COUNT;i++)
 	{
-		pthread_mutex_lock(&mutex1);
-		while (buffer1_is_full())
-			pthread_cond_wait(&wait_empty_buffer1, &mutex1);
+		//code signal
+		sema_wait(&empty_buffer1_sema);
+		sema_wait(&mutex_sema1);
 
 		item = 'a' + i;
 		put_item1(item);
 		printf("produce  item: %c\n",item);
 
-		pthread_cond_signal(&wait_full_buffer1);
-		pthread_mutex_unlock(&mutex1);
+		//code signal
+		sema_signal(&mutex_sema1);
+		sema_signal(&full_buffer1_sema);
 	}
 	return NULL;
 
@@ -153,13 +184,13 @@ int main()
 	pthread_t consumer_tid;
 	pthread_t calculator_tid;
 
-	pthread_mutex_init(&mutex1, NULL);
-	pthread_cond_init(&wait_empty_buffer1, NULL);
-	pthread_cond_init(&wait_full_buffer1, NULL);
+	sema_init(&mutex_sema1, 1);
+	sema_init(&empty_buffer1_sema, CAPACITY - 1);
+	sema_init(&full_buffer1_sema, 0);
 
-	pthread_mutex_init(&mutex2, NULL);
-	pthread_cond_init(&wait_empty_buffer2, NULL);
-	pthread_cond_init(&wait_full_buffer2, NULL);
+	sema_init(&mutex_sema2, 1);
+	sema_init(&empty_buffer2_sema, CAPACITY - 1);
+	sema_init(&full_buffer2_sema, 0);
 
 	pthread_create(&calculator_tid, NULL, calculate, NULL);
 	pthread_create(&consumer_tid, NULL, consume, NULL);
@@ -168,16 +199,3 @@ int main()
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
